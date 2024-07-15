f54747592fd8e924368fc600f5135b8b7e482351
RISHAB <rishavrajverma44@gmail.com>
2024-07-15 23:48:56 +0530

    image genrator

--- client/src/page/index.js
+++ client/src/page/index.js
@@ -1,7 +1,3 @@
-import Home from './Home';
-import CreatePost from './CreatePost';
+import Home from "./Home";
 
-export {
-  Home,
-  CreatePost,
-};
+export { Home };
--- server/mongodb/connect.js
+++ server/mongodb/connect.js
@@ -1,11 +1,12 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const connectDB = (url) => {
-  mongoose.set('strictQuery', true);
-  mongoose.connect(url)
-    .then(() => console.log('connected to mongo'))
+  mongoose.set("strictQuery", true);
+  mongoose
+    .connect(url)
+    .then(() => console.log("connected to mongo"))
     .catch((err) => {
-      console.error('failed to connect with mongo');
+      console.error("failed to connect with mongo");
       console.error(err);
     });
 };
