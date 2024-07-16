779be6172e09ef85fca4ae8f2c8679a5e3b622b4
RISHAB <rishavrajverma44@gmail.com>
2024-07-16 23:07:27 +0530

    adding

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -3,21 +3,23 @@ import React from "react";
 import { download } from "../assets";
 import { downloadImage } from "../utils";
 
-const Card = ({ id, alt, src }) => (
+const Card = ({ id, url }) => (
   <div className="rounded-xl group relative shadow-card hover:shadow-cardhover card">
     <img
       className="w-full h-auto object-cover rounded-xl"
-      src={src.original}
-      alt={alt}
+      src={`https://honest-jobs-like.loca.lt/${url}`}
+      alt={`image_${id + 1}`}
     />
     <div className="group-hover:flex flex-col max-h-[94.5%] hidden absolute bottom-0 left-0 right-0 bg-[#10131f] m-2 p-4 rounded-md">
       <div className="mt-5 flex justify-between items-center gap-2">
         <div className="flex items-center gap-2">
-          <p className="text-white text-sm">{alt}</p>
+          <p className="text-white text-sm">{`image_${id + 1}`}</p>
         </div>
         <button
           type="button"
-          onClick={() => downloadImage(id, src.original)}
+          onClick={() =>
+            downloadImage(id, `https://honest-jobs-like.loca.lt/${url}`)
+          }
           className="outline-none bg-transparent border-none"
         >
           <img
--- client/src/page/Home.jsx
+++ client/src/page/Home.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-
 import { Card, FormField, Loader } from "../components";
+import axios from "axios";
 
 const RenderCards = ({ data }) => {
   if (data?.length > 0) {
@@ -10,38 +10,82 @@ const RenderCards = ({ data }) => {
 
 const Home = () => {
   const [loading, setLoading] = useState(false);
-
   const [searchText, setSearchText] = useState("");
   const [searchedResults, setSearchedResults] = useState(null);
+  const [imageData, setImageData] = useState(null);
+  const [uploadedImage, setUploadedImage] = useState(null);
 
-  const fetchPosts = async () => {
+  const sendImage = async () => {
     setLoading(true);
+    console.log(imageData.get("file"));
 
     try {
-      const response = await fetch(
-        `https://api.pexels.com/v1/search?query=${searchText}`,
-        {
-          method: "GET",
-          headers: {
-            Authorization:
-              "Fn6U2vlWBhbPJ8qKc2gYBdCi2hOZD8mDmZJdcWIB0i9HBoRND7UiOfqN",
-          },
-        }
+      const respons = await axios.post(
+        "https://honest-jobs-like.loca.lt/process_images",
+        imageData
       );
-
-      if (response.status == 200) {
-        const result = await response.json();
-        setSearchedResults(result.photos);
+      console.log(respons);
+      if (respons.status === 200) {
+        pollGeneratedImage();
+      } else {
+        console.log("not success");
       }
     } catch (err) {
+      console.log("errr" + err);
       alert(err);
     } finally {
       setLoading(false);
     }
   };
 
-  const handleSearchChange = (e) => {
-    setSearchText(e.target.value);
+  const pollGeneratedImage = async () => {
+    let attempts = 0;
+
+    const poll = async () => {
+      try {
+        const response = await axios.get(
+          "https://honest-jobs-like.loca.lt/get_images"
+        );
+        if (response.status === 200) {
+          console.log("Generated image is ready");
+          console.log(response.data);
+          setSearchedResults(response.data);
+          setLoading(false);
+        } else if (attempts < 10) {
+          attempts++;
+          setTimeout(poll, 2000);
+        } else {
+          console.log(
+            "Failed to get generated image within the timeout period"
+          );
+          setLoading(false);
+        }
+      } catch (err) {
+        console.error("Error polling for generated image: ", err);
+        setLoading(false);
+      }
+    };
+
+    poll();
+  };
+
+  const handleChange = (e) => {
+    if (e.target.type === "text") {
+      setSearchText(e.target.value);
+    } else if (e.target.type === "file") {
+      const file = e.target.files[0];
+      if (file) {
+        setSearchedResults(null);
+        const imageData = new FormData();
+        imageData.append("file", file);
+        setImageData(imageData);
+        const reader = new FileReader();
+        reader.onloadend = () => {
+          setUploadedImage(reader.result);
+        };
+        reader.readAsDataURL(file);
+      }
+    }
   };
 
   return (
@@ -54,24 +98,64 @@ const Home = () => {
           The Pexels API enables programmatic access to the full Pexels content
           library, including photos, videos. All content is available free of
           charge, and you are welcome to use Pexels content for anything you'd
-          like
+          like.
         </p>
       </div>
 
       <div className="mt-16">
-        <FormField
-          labelName="Search posts"
-          type="text"
-          name="text"
-          placeholder="Search something..."
-          value={searchText}
-          handleChange={handleSearchChange}
-        />
+        <div className="flex bg-[#EcECF8] p-2">
+          <label
+            htmlFor="file_input"
+            className="flex-col p-2 items-center justify-center border-2 border-dashed rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100"
+          >
+            <svg
+              className="w-10 h-5 text-gray-400"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fillRule="evenodd"
+                d="M4 3a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V5a2 2 0 00-2-2H4zm8 7h-3v3a1 1 0 11-2 0v-3H6a1 1 0 010-2h3V6a1 1 0 112 0v2h3a1 1 0 010 2z"
+                clipRule="evenodd"
+              ></path>
+            </svg>
+            <p className="text-sm text-gray-800">Click to upload</p>
+            <input
+              id="file_input"
+              type="file"
+              className="hidden"
+              name="file"
+              onChange={handleChange}
+            />
+          </label>
+          <div className="flex-1 m-2">
+            <FormField
+              type="text"
+              name="text"
+              placeholder="Search something..."
+              value={searchText}
+              handleChange={handleChange}
+            />
+          </div>
+        </div>
+        {uploadedImage && (
+          <div className="mt-5">
+            <h3 className="text-sm font-medium text-gray-700">
+              Image Preview:
+            </h3>
+            <img
+              src={uploadedImage}
+              alt="Uploaded Preview"
+              className="mt-2 w-32 h-32 object-cover rounded-lg"
+            />
+          </div>
+        )}
         <div className="mt-5 flex gap-5">
           <button
             type="button"
-            onClick={fetchPosts}
-            className=" text-white bg-green-700 font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+            onClick={sendImage}
+            className="text-white bg-green-700 font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center"
           >
             {loading ? "Generating..." : "Generate"}
           </button>
@@ -79,13 +163,6 @@ const Home = () => {
       </div>
 
       <div className="mt-10">
-        {searchText ? (
-          <></>
-        ) : (
-          <h2 className="font-bold text-[#6469ff] text-xl uppercase">
-            TYPE AND GENERATE
-          </h2>
-        )}
         {loading ? (
           <div className="flex justify-center items-center">
             <Loader />
